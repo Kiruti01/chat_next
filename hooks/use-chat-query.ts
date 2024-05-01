3a251e04e9f34fb04ec8a34976fe4449c1b0d953
Michael Kiruti <mikekirutic@gmail.com>
2024-05-01 20:12:10 +0300

    merge

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -8,52 +8,39 @@ interface ChatQueryProps {
   apiUrl: string;
   paramKey: "channelId" | "conversationId";
   paramValue: string;
-};
+}
 
 export const useChatQuery = ({
   queryKey,
   apiUrl,
   paramKey,
-  paramValue
+  paramValue,
 }: ChatQueryProps) => {
   const { isConnected } = useSocket();
 
   const fetchMessages = async ({ pageParam = undefined }) => {
-    const url = qs.stringifyUrl({
-      url: apiUrl,
-      query: {
-        cursor: pageParam,
-        [paramKey]: paramValue,
-      }
-    }, { skipNull: true });
+    const url = qs.stringifyUrl(
+      {
+        url: apiUrl,
+        query: {
+          cursor: pageParam,
+          [paramKey]: paramValue,
+        },
+      },
+      { skipNull: true }
+    );
 
     const res = await fetch(url);
     return res.json();
   };
 
-<<<<<<< HEAD
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    status,
-  } = useInfiniteQuery({
-    queryKey: [queryKey],
-    queryFn: fetchMessages,
-    getNextPageParam: (lastPage) => lastPage?.nextCursor,
-    refetchInterval: isConnected ? false : 1000,
-  });
-=======
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useInfiniteQuery({
       queryKey: [queryKey],
       queryFn: fetchMessages,
       getNextPageParam: (lastPage) => lastPage?.nextCursor,
       refetchInterval: isConnected ? false : 1000,
-      // initialPageParam: undefined, // Or some other value if there's a better one
     });
->>>>>>> 221e631183579bd140dbe509ee423df5913473b6
 
   return {
     data,
@@ -62,4 +49,4 @@ export const useChatQuery = ({
     isFetchingNextPage,
     status,
   };
-}
+};
