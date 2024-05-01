91a6af2fc87f113ba0af6bbe9c3fa421b54a15e3
Michael Kiruti <mikekirutic@gmail.com>
2024-05-01 19:56:13 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -3,23 +3,17 @@ const nextConfig = {
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil"
+      bufferutil: "commonjs bufferutil",
     });
 
     return config;
   },
   images: {
-<<<<<<< HEAD
-    domains: [
-      "uploadthing.com",
-      "utfs.io"
-    ]
-  }
-}
-=======
     domains: ["uploadthing.com", "utfs.io"],
   },
 };
->>>>>>> 221e631183579bd140dbe509ee423df5913473b6
+//     domains: ["uploadthing.com", "utfs.io"],
+//   },
+// };
 
-module.exports = nextConfig
+module.exports = nextConfig;
